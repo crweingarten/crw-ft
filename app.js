fdf84a2a3f8c9085917486e2d463637d4d71d091
crweingarten <65681769+crweingarten@users.noreply.github.com>
2021-05-25 10:55:52 -0700

    saving work

--- app.js
+++ app.js
@@ -1,9 +1,34 @@
-const http = require('http')
-const fs = require('fs')
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'content-type': 'text/html' })
-  fs.createReadStream('index.html').pipe(res)
-})
+http.createServer(function(req, res){
 
-server.listen(process.env.PORT || 3000)
+    if(req.url === "/"){
+        fs.readFile("./index.html", "UTF-8", function(err, html){
+            res.writeHead(200, {"Content-Type": "text/html"});
+            res.end(html);
+        });
+    }else if(req.url.match("\.css$")){
+        var cssPath = path.join(__dirname, req.url);
+        var fileStream = fs.createReadStream(cssPath, "UTF-8");
+        res.writeHead(200, {"Content-Type": "text/css"});
+        fileStream.pipe(res);
+
+    }else if(req.url.match("\.js$")){
+        var jsPath = path.join(__dirname, req.url);
+        var fileStream = fs.createReadStream(jsPath);
+        res.writeHead(200, {"Content-Type": "text/javascript"});
+        fileStream.pipe(res)
+
+    }else if(req.url.match("\.png$")){
+        var imagePath = path.join(__dirname, req.url);
+        var fileStream = fs.createReadStream(imagePath);
+        res.writeHead(200, {"Content-Type": "image/png"});
+        fileStream.pipe(res);
+    }else{
+        res.writeHead(404, {"Content-Type": "text/html"});
+        res.end("No Page Found");
+    }
+
+}).listen(3000);
